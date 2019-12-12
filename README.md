0b0dd7872219aaa363bd703395d969f195663246
Schirrms <schirrms@schirrms.net>
2019-12-12 19:26:09 +0100

    Initial Version (0.1.0.0)

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# iTop-Rack-Cabling-To-Datacenter-View
-Just a small extension to integrate my Rack Cabling with Molkobain's DatacenterView
+# iTop-Generic-Connection-To-Datacenter-View
+Just a very small extension to complete Generic-Connection with Molkobain's Datacenter View
+
+# Goal
+
+This is a tiny extension, just adding some fields of Molkobain's [DatacenterView extension](https://github.com/Molkobain/itop-datacenter-view) in my own [iTop-Rack-Cabling extension](https://github.com/Schirrms/iTop-Rack-Cabling).
+
+# Dependencies
+
+Of course, this extension can only be installed on a system with :
+
+- Molkobain's DatacenterView >= 1.5.1
+- iTop-Rack-Cabling >= 0.2.0
+
+# Installation
+
+As for all my extensions, just download the zip file, and copy the 'schirrms-...' directory in your extensions directory, then rerun the setup as usual.
---
+++ schirrms-rack-cabling-to-datacenter-view/module.schirrms-rack-cabling-to-datacenter-view.php
@@ -0,0 +1,48 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'schirrms-rack-cabling-to-Datacenter-view/0.1.0',
+	array(
+		// Identification
+		//
+		'label' => 'Extensions for integrating Schirrms-Rack-Cabling to Molkobain-Datacenter-View',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'schirrms-rack-cabling/0.2.0',
+			'molkobain-datacenter-view/1.5.1'
+		),
+		'mandatory' => false,
+		'visible' => true,
+		'datamodel' => array(
+			'model.schirrms-generic-connection-to-datacenter-view.php'
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+		),
+	)
+);
+?>
